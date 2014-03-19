c4b618175c77bb85bcd9e487de883a152b217768
Sebatian Schilling <sebastian.schilling@rwth-aachen.de>
2014-03-19 16:32:49 +0100

    save current version of data privacy to user db after registration

--- testmaker4/protected/modules/user/controllers/RegistrationController.php
+++ testmaker4/protected/modules/user/controllers/RegistrationController.php
@@ -44,6 +44,7 @@ class RegistrationController extends Controller
 						$model->password=UserModule::encrypting($model->password);
 						$model->verifyPassword=UserModule::encrypting($model->verifyPassword);
 						$model->superuser=0;
+						$model->dataprivacyStatementAccepted = System::getValue(System::DATAPRIVACY_CURRENT_VERSION);
 						$model->status=((Yii::app()->controller->module->activeAfterRegister)?User::STATUS_ACTIVE:User::STATUS_NOACTIVE);
 						
 						if ($model->save()) {
